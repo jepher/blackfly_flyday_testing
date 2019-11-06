ead4a8eeb37e9ed7f1682575d2f378c2ef875e35
Jas <jjanda98@gmail.com>
2019-11-05 20:32:33 -0500

    refactor

--- blackfly_downscaler.py
+++ blackfly_downscaler.py
@@ -1,48 +1,81 @@
 import PySpin
 import os
-import imgurUrlGenerator
+import io
 import timeit
 from PIL import Image
 import numpy as np
-from skimage import transform 
-import imageio
+from cv2 import resize as resizeImage
+import cv2
 import sys, getopt
 import argparse
+import requests
 
-NUM_IMAGES = 1
-save_folder = "D:\Documents\AIAA"
+'''
+Used during fly days to test capture & preprocesing times and transmission times.
+    Flags:
+        apply_downscale: Should the image be downscaled?
+        framerate: Capturing framerate
+        num_images: Number of images to capture
+        image_dir: Image output directory
+'''
+IMAGE_DIR = "./"
+IP_ADDRESS = "http://192.168.1.127:8000";
+
+def sendtoserver(frame):
+    imencoded = cv2.imencode(".jpg", frame)[1]
+    headers = {"Content-type": "text/plain"}
+    try:
+        conn.request("POST", "/", imencoded.tostring(), headers)
+        response = conn.getresponse()
+    except conn.timeout as e:
+        print("timeout")
+
+
+    return response
 
 def capture(cam, apply_downscale, framerate, num_images):
-    cam.Init() # start camera
 
+    cam.Init() # start camera
     cam.AcquisitionMode.SetValue(PySpin.AcquisitionMode_Continuous) # set acquisition mode to continuous
     cam.AcquisitionFrameRateEnable.SetValue(True)
     cam.AcquisitionFrameRate.SetValue(int(framerate))
     cam.BeginAcquisition()
-
     for i in range(int(num_images)):
-    # while(True):
         start = timeit.default_timer()
         image = cam.GetNextImage() # acquire image
-
         if image.IsIncomplete():
             print("Image incomplete with image status %d..." % image.GetImageStatus())
         else: 
-            if(apply_downscale):
+            if(apply_downscale == 'True'):
                 image_array = image.Convert(PySpin.PixelFormat_RGB8, PySpin.NEAREST_NEIGHBOR).GetNDArray() # turn image into ND array
-                image_result = transform.downscale_local_mean(image_array, (5, 5, 1)).astype(np.uint8) # downscale image
-                 # print runtime
+                image_result = resizeImage(image_array, (500,500)) # downscale image
+                # print runtime
                 stop = timeit.default_timer()
-                print('Time: ', stop - start) 
-                imageio.imsave('{}/image{}.jpg'.format(save_folder, i), image_result) # save image
+                print('Processing Time: ', stop - start)
+                buff = io.BytesIO()
+                start = timeit.default_timer()
+                img_encoded = cv2.imencode('.jpg', image_result)[1]
+                response = requests.post(IP_ADDRESS, data=img_encoded.tostring())
+                done = response.status_code
+                stop = timeit.default_timer()
+                print('Transmission Time: ', stop - start)
             else:
-                image_result = image.Convert(PySpin.PixelFormat_RGB8, PySpin.NEAREST_NEIGHBOR)
-                 # print runtime
+                image_array = image.Convert(PySpin.PixelFormat_RGB8, PySpin.NEAREST_NEIGHBOR).GetNDArray()
+                image_result = resizeImage(image_array, (1000,1000)) # downscale image
                 stop = timeit.default_timer()
-                print('Time: ', stop - start) 
-                qual = PySpin.JPEGOption()
-                qual.quality = 100
-                image_result.Save('{}/image{}.jpg'.format(save_folder, i), qual)
+                print('Processing Time: ', stop - start)
+                buff = io.BytesIO()
+                start = timeit.default_timer()
+                img_encoded = cv2.imencode('.jpg', image_result)[1]
+                response = requests.post(IP_ADDRESS, data=img_encoded.tostring())
+                done = response.status_code
+                stop = timeit.default_timer()
+                print('Transmission Time: ', stop - start)
+                #qual = PySpin.JPEGOption()
+                #qual.quality = 100
+                #filename = os.path.join("/home/nvidia/trial/image{}.jpg".format(i))
+                #print(filename)
+                #image_result.Save(filename, qual)
             
 
     # deinitialize camera   
@@ -50,34 +83,24 @@ def capture(cam, apply_downscale, framerate, num_images):
     cam.DeInit()
     del cam
 
-def main(apply_downscale, framerate, num_images):
-    # parse arguments
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("apply_downscale")
-    # parser.add_argument("framerate")
-
-    # args = parser.parse_args()
-    # apply_downscale = str(args.apply_downscale)
-    # framerate = str(args.framerate)
-
+def main(apply_downscale, framerate, num_images, image_dir):
+    IMAGE_DIR = image_dir
+    print(IMAGE_DIR)
     system = PySpin.System.GetInstance()
     cam_list = system.GetCameras() # get list of cameras
     if cam_list.GetSize() == 0: # exit if no cameras detected
         cam_list.Clear()
-
         system.ReleaseInstance()
         print("No cameras")
         input("Done! Press Enter to exit...")
         return False
-    
-    cam = system.GetCameras().GetByIndex(0) # get camera
+    cam = cam_list.GetByIndex(0) # get camera
     # configure_custom_image_settings(cam) # configure camera settings
     capture(cam, apply_downscale, framerate, num_images)
-
     # shut down
     cam_list.Clear()
     del cam
     system.ReleaseInstance()
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
