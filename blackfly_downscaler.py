0c548d0011285fd797a4835da39e992cfd34163a
Jeffrey Yang <jeffreyyang217@gmail.com>
2019-11-15 00:20:16 -0500

    authentication added

--- blackfly_downscaler.py
+++ blackfly_downscaler.py
@@ -1,79 +1,112 @@
 import PySpin
-import os
-import imgurUrlGenerator
 import timeit
-from PIL import Image
+import datetime
 import numpy as np
-from skimage import transform 
-import imageio
-import sys, getopt
-import argparse
-
-NUM_IMAGES = 1
-save_folder = "D:\Documents\AIAA"
+from cv2 import resize as resizeImage
+import cv2
+import sys
+import requests, json
+'''
+Used during fly days to test capture & preprocesing times and transmission times.
+    Flags:
+        apply_downscale: Should the image be downscaled?
+        framerate: Capturing framerate
+        num_images: Number of images to capture
+        image_dir: Image output directory
+'''
+IMAGE_DIR = "./"
+IP_ADDRESS = "http://172.31.152.7:8080"
+USERNAME = "drone"
+PASSWORD = "ruautonomous"
+token_label = "Authorization"
+access_token = None
 
 def capture(cam, apply_downscale, framerate, num_images):
-    cam.Init() # start camera
 
+    cam.Init() # start camera
     cam.AcquisitionMode.SetValue(PySpin.AcquisitionMode_Continuous) # set acquisition mode to continuous
     cam.AcquisitionFrameRateEnable.SetValue(True)
     cam.AcquisitionFrameRate.SetValue(int(framerate))
     cam.BeginAcquisition()
-
     for i in range(int(num_images)):
-    # while(True):
-        start = timeit.default_timer()
+        start = timeit.default_timer() # start downscale timer
         image = cam.GetNextImage() # acquire image
-
         if image.IsIncomplete():
             print("Image incomplete with image status %d..." % image.GetImageStatus())
         else: 
-            if(apply_downscale):
-                image_array = image.Convert(PySpin.PixelFormat_RGB8, PySpin.NEAREST_NEIGHBOR).GetNDArray() # turn image into ND array
-                image_result = transform.downscale_local_mean(image_array, (5, 5, 1)).astype(np.uint8) # downscale image
-                 # print runtime
-                stop = timeit.default_timer()
-                print('Time: ', stop - start) 
-                imageio.imsave('{}/image{}.jpg'.format(save_folder, i), image_result) # save image
+            image_array = image.Convert(PySpin.PixelFormat_RGB8, PySpin.NEAREST_NEIGHBOR).GetNDArray() # turn image into ND array
+            if(apply_downscale == 'True'):
+                image_result = resizeImage(image_array, (500,500)) # downscale image
             else:
-                image_result = image.Convert(PySpin.PixelFormat_RGB8, PySpin.NEAREST_NEIGHBOR)
-                 # print runtime
+                image_result = resizeImage(image_array, (1000,1000)) # downscale image
+            # print runtime
+            stop = timeit.default_timer()
+            print('Processing Time: ', stop - start)
+            start = timeit.default_timer()
+            img_encoded = cv2.imencode('.jpg', image_result)[1]
+
+            start = timeit.default_timer() # start transmission timer
+            access_token = connect()
+            headers = {token_label : access_token}
+            # files = {"image": img_encoded.tostring()}
+            while True:
+                try:
+                    with open("D:/Downloads/75380261_1573374882801635_2123551660729958400_n.jpg", 'rb') as f:
+                        contents = f.read()
+                    files = {"image": contents}
+                    break
+                except Exception as e:
+                    continue
+            telem_data = {name: (None,"1") for name in ["pitch", "roll", "lat", "lon", "alt", "rel_alt", "yaw", "hdg"]}
+            telem_data["time_saved"] = datetime.datetime.now().strftime("%H:%M:%S")
+            telem_data["time_sent"] = datetime.datetime.now().strftime("%H:%M:%S")
+            print(telem_data)
+            response = requests.post(IP_ADDRESS + "/drone/postImage", headers = headers, files = files, data = telem_data)
+            done = response.status_code
+            if done == 200:
                 stop = timeit.default_timer()
-                print('Time: ', stop - start) 
-                qual = PySpin.JPEGOption()
-                qual.quality = 100
-                image_result.Save('{}/image{}.jpg'.format(save_folder, i), qual)
-            
+                print('Transmission Time: ', stop - start)    
+            else:
+                print('Transmission error: ' + str(done))         
 
     # deinitialize camera   
     cam.EndAcquisition()
     cam.DeInit()
     del cam
 
+def connect():
+    logged_in = False
+    endpoint = IP_ADDRESS + "/drone/login"
+    headers = {"Content-Type": "application/json; charset=UTF-8"}
+    data = {"password": PASSWORD, "username": USERNAME}
+
+    while logged_in == False:
+        try:
+            response = requests.post(endpoint, headers = headers, data = json.dumps(data))
+
+            if response.status_code == 200:
+                access_token = "JWT " + response.json()["token"]
+                logged_in = True
+        # except TypeError:
+        #     # the JWT returned by the CCS sometimes is not correct Base64, thus we keep trying until we get a valid one
+        #     pass
+        except KeyboardInterrupt:
+            return
+
+    print("DEBUG: Successfully logged in to " + endpoint + " at " + str(datetime.datetime.now().time()))
+    return access_token
+
 def main(apply_downscale, framerate, num_images):
-    # parse arguments
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("apply_downscale")
-    # parser.add_argument("framerate")
-
-    # args = parser.parse_args()
-    # apply_downscale = str(args.apply_downscale)
-    # framerate = str(args.framerate)
-
     system = PySpin.System.GetInstance()
     cam_list = system.GetCameras() # get list of cameras
     if cam_list.GetSize() == 0: # exit if no cameras detected
         cam_list.Clear()
-
         system.ReleaseInstance()
         print("No cameras")
         input("Done! Press Enter to exit...")
         return False
-    
-    cam = system.GetCameras().GetByIndex(0) # get camera
-    # configure_custom_image_settings(cam) # configure camera settings
+    cam = cam_list.GetByIndex(0) # get camera
     capture(cam, apply_downscale, framerate, num_images)
-
     # shut down
     cam_list.Clear()
     del cam
