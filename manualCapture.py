00c2a32af87f94d88f651c134b63a18f9dd8bcc1
Jeffrey Yang <jeffreyyang217@gmail.com>
2019-11-19 21:50:46 -0500

    manual trigger

---
+++ manualCapture.py
@@ -0,0 +1,324 @@
+import os
+import PySpin
+
+NUM_IMAGES = 1  # number of images to grab
+
+
+class TriggerType:
+    SOFTWARE = 1
+    HARDWARE = 2
+
+
+CHOSEN_TRIGGER = TriggerType.SOFTWARE
+
+
+def configure_trigger(cam):
+    """
+    Configures camera to use a trigger. First, trigger mode is
+    set to off in order to select the trigger source. Once trigger source
+    is selected, trigger mode is then enabled, which has the camera
+    capture only a single image upon execution of chosen trigger.
+
+     :param cam: Camera to configure trigger for.
+     :type cam: CameraPtr
+     :return: True if successful, False otherwise.
+     :rtype: bool
+    """
+    result = True
+
+    print('*** CONFIGURING TRIGGER ***\n')
+
+    if CHOSEN_TRIGGER == TriggerType.SOFTWARE:
+        print('Software trigger chosen ...')
+    elif CHOSEN_TRIGGER == TriggerType.HARDWARE:
+        print('Hardware trigger chose ...')
+
+    try:
+        # Ensure trigger mode off
+        # The trigger must be disabled in order to configure whether the source
+        # is software or hardware.
+        nodemap = cam.GetNodeMap()
+        node_trigger_mode = PySpin.CEnumerationPtr(nodemap.GetNode('TriggerMode'))
+
+        node_trigger_mode_off = node_trigger_mode.GetEntryByName('Off')
+
+        node_trigger_mode.SetIntValue(node_trigger_mode_off.GetValue())
+
+        print('Trigger mode disabled...')
+
+        # Select trigger source
+        # The trigger source must be set to hardware or software while trigger
+        # mode is off.
+        node_trigger_source = PySpin.CEnumerationPtr(nodemap.GetNode('TriggerSource'))
+
+        if CHOSEN_TRIGGER == TriggerType.SOFTWARE:
+            node_trigger_source_software = node_trigger_source.GetEntryByName('Software')
+            node_trigger_source.SetIntValue(node_trigger_source_software.GetValue())
+
+        elif CHOSEN_TRIGGER == TriggerType.HARDWARE:
+            node_trigger_source_hardware = node_trigger_source.GetEntryByName('Line0')
+            node_trigger_source.SetIntValue(node_trigger_source_hardware.GetValue())
+
+        # Turn trigger mode on
+        # Once the appropriate trigger source has been set, turn trigger mode
+        # on in order to retrieve images using the trigger.
+        node_trigger_mode_on = node_trigger_mode.GetEntryByName('On')
+
+        node_trigger_mode.SetIntValue(node_trigger_mode_on.GetValue())
+        print('Trigger mode turned back on...')
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        return False
+
+    return result
+
+
+def grab_next_image_by_trigger(nodemap, cam):
+    """
+    This function acquires an image by executing the trigger node.
+
+    :param cam: Camera to acquire images from.
+    :param nodemap: Device nodemap.
+    :type cam: CameraPtr
+    :type nodemap: INodeMap
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+    try:
+        result = True
+        # Use trigger to capture image
+        # The software trigger only feigns being executed by the Enter key;
+        # what might not be immediately apparent is that there is not a
+        # continuous stream of images being captured; in other examples that
+        # acquire images, the camera captures a continuous stream of images.
+        # When an image is retrieved, it is plucked from the stream.
+
+        if CHOSEN_TRIGGER == TriggerType.SOFTWARE:
+            # Get user input
+            input('Press the Enter key to initiate software trigger.')
+
+            # Execute software trigger
+            node_softwaretrigger_cmd = PySpin.CCommandPtr(nodemap.GetNode('TriggerSoftware'))
+            node_softwaretrigger_cmd.Execute()
+
+            # TODO: Blackfly and Flea3 GEV cameras need 2 second delay after software trigger
+
+        elif CHOSEN_TRIGGER == TriggerType.HARDWARE:
+            print('Use the hardware to trigger image acquisition.')
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        return False
+
+    return result
+
+
+def acquire_images(cam, nodemap, nodemap_tldevice):
+    """
+    :param cam: Camera to acquire images from.
+    :param nodemap: Device nodemap.
+    :param nodemap_tldevice: Transport layer device nodemap.
+    :type cam: CameraPtr
+    :type nodemap: INodeMap
+    :type nodemap_tldevice: INodeMap
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+
+    print('*** IMAGE ACQUISITION ***\n')
+    try:
+        result = True
+
+        # Set acquisition mode to continuous
+        # In order to access the node entries, they have to be casted to a pointer type (CEnumerationPtr here)
+        node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionMode'))
+
+        # Retrieve entry node from enumeration node
+        node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
+
+        # Retrieve integer value from entry node
+        acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
+
+        # Set integer value from entry node as new value of enumeration node
+        node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
+
+        print('Acquisition mode set to continuous...')
+
+        #  Begin acquiring images
+        cam.BeginAcquisition()
+
+        print('Acquiring images...')
+
+        # Retrieve, convert, and save images
+        for i in range(NUM_IMAGES):
+            try:
+
+                #  Retrieve the next image from the trigger
+                result &= grab_next_image_by_trigger(nodemap, cam)
+
+                #  Retrieve next received image
+                image_result = cam.GetNextImage()
+
+                #  Ensure image completion
+                if image_result.IsIncomplete():
+                    print('Image incomplete with image status %d ...' % image_result.GetImageStatus())
+
+                else:
+                    image_converted = image_result.Convert(PySpin.PixelFormat_RGB8, PySpin.NEAREST_NEIGHBOR)
+
+                    # Create a unique filename
+                    filename = 'Trigger-%d.jpg' % i
+
+                    # Save image
+                    image_converted.Save(filename)
+                    print('Image saved at %s\n' % filename)
+
+                    #  Release image
+                    #
+                    #  *** NOTES ***
+                    #  Images retrieved directly from the camera (i.e. non-converted
+                    #  images) need to be released in order to keep from filling the
+                    #  buffer.
+                    image_result.Release()
+
+            except PySpin.SpinnakerException as ex:
+                print('Error: %s' % ex)
+                return False
+
+        # End acquisition
+        cam.EndAcquisition()
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        return False
+
+    return result
+
+
+def reset_trigger(nodemap):
+    """
+    This function returns the camera to a normal state by turning off trigger mode.
+  
+    :param nodemap: Transport layer device nodemap.
+    :type nodemap: INodeMap
+    :returns: True if successful, False otherwise.
+    :rtype: bool
+    """
+    try:
+        result = True
+        node_trigger_mode = PySpin.CEnumerationPtr(nodemap.GetNode('TriggerMode'))
+
+        node_trigger_mode_off = node_trigger_mode.GetEntryByName('Off')
+
+        node_trigger_mode.SetIntValue(node_trigger_mode_off.GetValue())
+
+        print('Trigger mode disabled...')
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        result = False
+
+    return result
+
+def run_single_camera(cam):
+    """
+    :param cam: Camera to run on.
+    :type cam: CameraPtr
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+    try:
+        result = True
+        
+        # Retrieve TL device nodemap
+        nodemap_tldevice = cam.GetTLDeviceNodeMap()
+
+        # Initialize camera
+        cam.Init()
+
+        # Retrieve GenICam nodemap
+        nodemap = cam.GetNodeMap()
+
+        # Configure trigger
+        if configure_trigger(cam) is False:
+            return False
+
+        # Acquire images
+        result &= acquire_images(cam, nodemap, nodemap_tldevice)
+
+        # Reset trigger
+        result &= reset_trigger(nodemap)
+
+        # Deinitialize camera
+        cam.DeInit()
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        result = False
+
+    return result
+
+
+def main():
+    """
+    Example entry point; please see Enumeration example for more in-depth
+    comments on preparing and cleaning up the system.
+
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+
+    # Since this application saves images in the current folder
+    # we must ensure that we have permission to write to this folder.
+    # If we do not have permission, fail right away.
+    try:
+        test_file = open('test.txt', 'w+')
+    except IOError:
+        print('Unable to write to current directory. Please check permissions.')
+        input('Press Enter to exit...')
+        return False
+
+    test_file.close()
+    os.remove(test_file.name)
+
+    result = True
+
+    # Retrieve singleton reference to system object
+    system = PySpin.System.GetInstance()
+
+    # Retrieve list of cameras from the system
+    cam_list = system.GetCameras()
+
+    num_cameras = cam_list.GetSize()
+
+    # Finish if there are no cameras
+    if num_cameras == 0:
+        # Clear camera list before releasing system
+        cam_list.Clear()
+
+        # Release system instance
+        system.ReleaseInstance()
+
+        print('No cameras detected!')
+        input('Done! Press Enter to exit...')
+        return False
+
+    cam = cam_list.GetByIndex(0)
+    result &= run_single_camera(cam)
+
+    # Release reference to camera
+    del cam
+
+    # Clear camera list before releasing system
+    cam_list.Clear()
+
+    # Release system instance
+    system.ReleaseInstance()
+
+    input('Done! Press Enter to exit...')
+    return result
+
+
+if __name__ == '__main__':
+    main()
